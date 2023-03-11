bde62bc776801d7762cc5bd781dead064ad4965e
Mathew Elston <elstonmathew@yahoo.com>
2023-03-10 18:06:29 -0800

    Updated Main to get user input

--- Main.java
+++ Main.java
@@ -1,19 +1,39 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
         String connectionString = "jdbc:mysql://localhost:3306/coffee_db";
-        String tableName = "coffee";
+        String tableName = "customer";
         String columns[] = {
-            "product_name",
-            "product_price"
+            "name",
+            "phone",
+            "email"
         };
-        String values[] = {
-            "columbia",
-            "$2.99"
-        };
-        String value = "Seattles Best";
-        
         SQLDataBase database = new SQLDataBase(tableName, connectionString, "root", "");
+
+        System.out.println("How many items would you like to add to the table?");
+        int valueSize = keyboard.nextInt();
+        keyboard.nextLine();
+        String values[] = new String [valueSize * columns.length];
+        int index = 0;
+        for (int i = 0; i < valueSize -1; i++) {
+            System.out.println("Customer Name?");
+            String customerName = keyboard.nextLine();
+            values[index] = customerName;
+            index++;
+
+            System.out.println("Customer phone number?");
+            String customerPhone = keyboard.nextLine();
+            values[index] = customerPhone;
+            index++;
+
+            System.out.println("Customer Email?");
+            String customerEmail = keyboard.nextLine();
+            values[index] = customerEmail;
+            index++;
+        }
         database.insert(columns, values);
-    }
-    
+        keyboard.close();
+    }    
 }
